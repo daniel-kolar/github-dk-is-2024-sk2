d9e527ff8fdc2a4a4ee6c192ef98e41b59179c7b
Daniel Kolář <daniel.kolar@tul.cz>
2024-10-10 12:52:09 +0200

    #1 .NET build

--- IS - projekty/program001-výpis-řady/Program.cs
+++ IS - projekty/program001-výpis-řady/Program.cs
@@ -2,9 +2,15 @@
 
 class Vypis_rady{
     static void Main(){
-        int prvni = 1;
+        Console.WriteLine("zadej 1. cislo: ");
+        int prvni = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("zadej cislo vetsi nez 1.: ");
+        int posledni = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("zadej cislo, ktere chces pricist: ");
+        int krok = Convert.ToInt32(Console.ReadLine());
+        /*int prvni = 1;
         int posledni = 10;
-        int krok = 2;
+        int krok = 2;*/
 
         for (int i = prvni; i <= posledni; i += krok){
             Console.WriteLine(i);
