ec592734d4719f1cb33c7097f115ccf7bb889d6d
Daniel Kolář <daniel.kolar@tul.cz>
2024-10-17 13:23:21 +0200

    #2 DONE

--- IS - projekty/program002a-soucet-cifer/Program.cs
+++ IS - projekty/program002a-soucet-cifer/Program.cs
@@ -1,10 +1,60 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
 
-class Soucet_cifer{
+class Vypis_rady{
     static void Main(){
-        //soucet dvou cifer čistě matematickým způsobem
-        int a = 2;
-        int b = 3;
-        Console.WriteLine(a+b);
+        
+        //chci aby se program opakoval po stisku klavesy a
+        string again = "a";
+        while(again == "a"){
+            //Console.Clear();
+            Console.WriteLine("*****************************");
+            Console.WriteLine("*** Součet a součin cifer ***");
+            Console.WriteLine("*****************************");
+            Console.WriteLine("******** Daniel Kolář *******");
+            Console.WriteLine("*****************************\n"); //\n new line nebo console.writeline()
+            Console.WriteLine();
+
+            //vstup od uzivatele - spatna varianta
+                //Console.Write("zadejte prvni cislo rady: ");
+                //int first = int.Parse(Console.ReadLine());
+
+            //vstup od uzivatele - lepsi varianta
+            Console.Write("zadejte cele cislo: ");
+            int number;
+            while(!int.TryParse(Console.ReadLine(), out number)){
+                Console.WriteLine("Nezadali jste cele cislo. Zadejte celé číslo: ");
+            }
+
+            //kontrola vstupni hodnoty
+            Console.WriteLine("============================");
+            Console.WriteLine("uzivatel zadal: {0}", number);
+            Console.WriteLine("============================\n\n");
+
+            int suma = 0; //iteracni suma inicializuju jako neutralni treba s scitanim 0
+            int numberBackup = number;
+            int digit;
+
+            //pokud je cislo zaporne zmenim ho na kladne
+            if(number < 0){
+                number = - number;
+            }
+
+            while(number >= 10){
+                digit = number % 10; //operator modulo(%) 123/10 = 12 a 3 - urceni zbytku po deleni cisle
+                number = (number - digit) / 10;
+                Console.WriteLine("Digit je {0}", digit);
+                suma = suma + digit;
+            }
+            Console.WriteLine("Digit je {0}", number);
+            suma = suma + number;
+
+            Console.WriteLine("\n\nSoucet cifer cisla {0} je {1}", numberBackup, suma);
+
+            //opakovani programu - TO-DO
+            Console.WriteLine("pro opakovani programu stisknete klavesu a");
+            again = Console.ReadLine();
+        }
     }
 }
