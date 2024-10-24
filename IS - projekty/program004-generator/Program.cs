71809110140fede68b320b0b20eb701692b4d382
Daniel Kolář <daniel.kolar@tul.cz>
2024-10-24 13:06:50 +0200

    Úkol 004 - generátor pseudonáhodných čísel #5 - uzivatelsky vstup

--- IS - projekty/program004-generator/Program.cs
+++ IS - projekty/program004-generator/Program.cs
@@ -9,23 +9,40 @@ class Vypis_rady{
         string again = "a";
         while(again == "a"){
             Console.Clear();
-            Console.WriteLine("*****************************");
-            Console.WriteLine("********* Generator *********");
-            Console.WriteLine("*****************************");
-            Console.WriteLine("******** Daniel Kolář *******");
-            Console.WriteLine("*****************************\n"); //\n new line nebo console.writeline()
+            Console.WriteLine("*************************************************");
+            Console.WriteLine("******** Generátor pseudonáhodných čísel ********");
+            Console.WriteLine("*************************************************");
+            Console.WriteLine("**************** Daniel Kolář *******************");
+            Console.WriteLine("***********************************************\n"); //\n new line nebo console.writeline()
             Console.WriteLine();
 
-            //vstup od uzivatele - spatna varianta
-                //Console.Write("zadejte prvni cislo rady: ");
-                //int first = int.Parse(Console.ReadLine());
-
             //vstup od uzivatele - lepsi varianta
-            Console.Write("zadejte první celé číslo řady: ");
-            int first;
-            while(!int.TryParse(Console.ReadLine(), out first)){
+            Console.Write("zadejte pocet generovanych cisel (cele cislo): ");
+            int n;
+            while(!int.TryParse(Console.ReadLine(), out n)){
                 Console.WriteLine("Nezadali jste cele cislo. Zadejte znovu první celé číslo řady: ");
             }
+
+            Console.Write("zadejte dolni mez (cele cislo): ");
+            int dm;
+            while(!int.TryParse(Console.ReadLine(), out dm)){
+                Console.WriteLine("Nezadali jste cele cislo. Zadejte znovu první celé číslo řady: ");
+            }
+
+            Console.Write("zadejte horni mez (cele cislo): ");
+            int hm;
+            while(!int.TryParse(Console.ReadLine(), out hm)){
+                Console.WriteLine("Nezadali jste cele cislo. Zadejte znovu první celé číslo řady: ");
+            }
+
+            //kontrola vstupu uzivatele
+            Console.WriteLine("\n\n=====================");
+            Console.WriteLine("Uživatelův vstup:");
+            Console.WriteLine("Pocet cisel: {0}; dolni mez: {1}; horni mez: {2}", n, dm, hm);
+            Console.WriteLine("=====================\n\n");
+
+
+
 
             //opakovani programu - TO-DO
             Console.WriteLine("pro opakovani programu stisknete klavesu a");
