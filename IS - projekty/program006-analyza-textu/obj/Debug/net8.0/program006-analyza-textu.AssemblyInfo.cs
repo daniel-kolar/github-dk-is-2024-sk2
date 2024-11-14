0b2fd2dcddef520278d152e8d9f2e84b8d71bbbf
Daniel Kolář <daniel.kolar@tul.cz>
2024-11-14 11:27:42 +0100

    Úkol 007 - Řazení pomocí Bubble sortu #8 - zalozeni

--- IS - projekty/program006-analyza-textu/obj/Debug/net8.0/program006-analyza-textu.AssemblyInfo.cs
+++ IS - projekty/program006-analyza-textu/obj/Debug/net8.0/program006-analyza-textu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program006-analyza-textu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ffb18ea0b175e57dcd86f2a80cad06ec6cf57e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a9a812c4f6b78c13bc3520dd967bd92882b94ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("program006-analyza-textu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program006-analyza-textu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ IS - projekty/program007-bubble-sort/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+
+class Vypis_rady{
+    static void Main(){
+        
+        //chci aby se program opakoval po stisku klavesy a
+        string again = "a";
+        while(again == "a"){
+            Console.Clear();
+            Console.WriteLine("*************************************************");
+            Console.WriteLine("******** Generátor pseudonáhodných čísel ********");
+            Console.WriteLine("*************************************************");
+            Console.WriteLine("**************** Daniel Kolář *******************");
+            Console.WriteLine("***********************************************\n"); //\n new line nebo console.writeline()
+            Console.WriteLine();
+
+            //vstup od uzivatele - lepsi varianta
+            Console.Write("zadejte pocet generovanych cisel (cele cislo): "); //obecny ukladani pro neznama data jsou dobre vyuzit arrays(pole)
+            int n;
+            while(!int.TryParse(Console.ReadLine(), out n)){
+                Console.WriteLine("Nezadali jste cele cislo. Zadejte znovu první celé číslo řady: ");
+            }
+
+            Console.Write("zadejte dolni mez (cele cislo): ");
+            int dm;
+            while(!int.TryParse(Console.ReadLine(), out dm)){
+                Console.WriteLine("Nezadali jste cele cislo. Zadejte znovu první celé číslo řady: ");
+            }
+
+            Console.Write("zadejte horni mez (cele cislo): ");
+            int hm;
+            while(!int.TryParse(Console.ReadLine(), out hm)){
+                Console.WriteLine("Nezadali jste cele cislo. Zadejte znovu první celé číslo řady: ");
+            }
+
+            //kontrola vstupu uzivatele
+            Console.WriteLine("\n\n=====================");
+            Console.WriteLine("Uživatelův vstup:");
+            Console.WriteLine("Pocet cisel: {0}; dolni mez: {1}; horni mez: {2}", n, dm, hm);
+            Console.WriteLine("=====================\n\n");
+
+            //deklarace array(pole) - vic suplicku pro data, ktery maji svoje indexy
+            int[] myArray = new int[n]; //UZITECNE - nauc se to (vis, ze ti pole nikdy nesli)
+            
+            //priprava pro generovani nahodnych cisel
+            Random randomNumber = new Random();
+
+            Console.WriteLine("náhodna cisla: ");
+
+            for(int i = 0; i < n; i++){
+                myArray[i] = randomNumber.Next(dm, hm + 1);
+                Console.WriteLine("{0}: ", myArray[i]);
+            }
+
+
+            //opakovani programu - TO-DO
+            Console.WriteLine("\n\npro opakovani programu stisknete klavesu a");
+            again = Console.ReadLine();
+        }
+    }
+}
