a703d1c63061f9e4e2b07e0a2e489d17966203ef
Daniel Kolář <daniel.kolar@tul.cz>
2024-11-20 12:21:33 +0100

    Úkol 008 - Další základní řadící metody - domácí úkol #9 - SHAKER SORT

--- IS - projekty/program008-radici-metody/Program.cs
+++ IS - projekty/program008-radici-metody/Program.cs
@@ -50,7 +50,7 @@ class Vypis_rady{
 
             Console.WriteLine("náhodna cisla: ");
 
-            for(int i = 0; i < n; i++){
+            for(int i = 0; i < n; i++){ //generace pole
                 myArray[i] = randomNumber.Next(dm, hm + 1);
                 Console.Write("{0}, ", myArray[i]);
             }
@@ -60,10 +60,10 @@ class Vypis_rady{
             //RADICI METODY
             Console.WriteLine("\n\n=====================");
             Stopwatch myStopwatch = new Stopwatch();
+
             
-
+           
             //SELECTION SORT
-
             int biggestNum = 0;
             myStopwatch.Start();
             for(int i = 0; i < n; i++){ //prochazim pole
@@ -88,17 +88,16 @@ class Vypis_rady{
 
 
             //INSERTION SORT
-
             myStopwatch.Reset();
             myStopwatch.Start();
-            for(int i = 0; i < n - 1; i++){
-                int j = i + 1;
-                int tmp = myArray[j];
-                while(j > 0 && tmp > myArray[j - 1]){
-                    myArray[j] = myArray[j - 1];
+            for(int i = 1; i < n - 1; i++){
+                int tmp = myArray[i];
+                int j = i - 1;
+                while(j >= 0 && tmp > myArray[j]){
+                    myArray[j + 1] = myArray[j];
                     j--;
                 }
-                myArray[j] = tmp;
+                myArray[j + 1] = tmp;
             }
             myStopwatch.Stop();
 
@@ -106,11 +105,49 @@ class Vypis_rady{
             for(int i = 0; i < n; i++){
                 Console.Write("{0}, ", myArray[i]);
             }
-            Console.WriteLine("\n\nCas Selection sort: {0} ms", myStopwatch.Elapsed);
+            Console.WriteLine("\n\nCas Insertion sort: {0} ms", myStopwatch.Elapsed);
 
 
-
+           
             //SHAKER SORT
+            myStopwatch.Reset();
+            myStopwatch.Start();
+
+            bool swapped = true;
+            int start = 0;
+            int end = n - 1;
+
+            while(swapped == true){
+                swapped = false; //reset pro pripad, ze se nic nezmění
+                for(int i = start; i < end; i++){ //projizdim od zdola nahoru stejne jako v selection sortu(bubblesort)
+                    if(myArray[i] < myArray[i + 1]){
+                        int tmp = myArray[i];
+                        myArray[i] = myArray[i + 1];
+                        myArray[i + 1] = tmp;
+                        swapped = true;
+                    }
+                }
+
+                if(swapped == false) break; //pokud se nic neprohodi, tak je pole uz serazene
+                swapped = false; //reset pro dalsi fazi
+                end--; //konec zmensim o jednu protoze posledni prvek je uz serazen
+
+                for(int i = end; i > start; i--){ //projizdim od zhora dolu
+                    if(myArray[i - 1] < myArray[i]){
+                        int tmp = myArray[i - 1];
+                        myArray[i - 1] = myArray[i];
+                        myArray[i] = tmp;
+                        swapped = true;
+                    }
+                }
+                start++; //zacatek posunu o jednu dolu, protoze prvni prvek je uz serazen
+            }
+            myStopwatch.Stop();
+            Console.WriteLine("\n\nSerazene pole pomoci Shaker sort:");
+            for(int i = 0; i < n; i++){
+                Console.Write("{0}, ", myArray[i]);
+            }
+            Console.WriteLine("\n\nCas Shaker sort: {0} ms", myStopwatch.Elapsed);
 
 
 
