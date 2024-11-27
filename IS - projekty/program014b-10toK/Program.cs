19ed507aac986f312678a80cf27abfc7282cb7dd
Daniel Kolář <daniel.kolar@tul.cz>
2024-11-27 20:52:54 +0100

    Úkol 014 - převod z 10 do 2 soustavy #14 - Prevod do jakekoliv soustavy SKORO DONE

--- IS - projekty/program014b-10toK/Program.cs
+++ IS - projekty/program014b-10toK/Program.cs
@@ -17,33 +17,42 @@ class Vypis_rady{
             Console.WriteLine();
 
             //vstup od uzivatele - lepsi varianta
-            Console.Write("zadejte celé číslo, které chcete převest do binárního soustavy: ");
+            Console.Write("zadejte celé číslo, které chcete převest do vámi zadané soustavy: ");
             int decimalNumber;
             while(!int.TryParse(Console.ReadLine(), out decimalNumber)){
                 Console.WriteLine("Nezadali jste cele cislo. Zadejte znovu celé číslo, které chcete převest do binárního soustavy: ");
             }
 
-            Console.Write("{0} v binární soustavě je: ", decimalNumber);
+            Console.Write("Zadejte soustavu do které chcete převádět: ");
+            int baseNumber;
+            while(!int.TryParse(Console.ReadLine(), out baseNumber)){
+                Console.WriteLine("Nezadali jste cele cislo soustavy. Zadejte znovu cele cislo soustavy: ");
+            }
+
+            Console.Write("{0} v/ve {1} soustavě je: ", decimalNumber, baseNumber);
 
             //Prevod do binarni soustavy a ulozeni do pole
-            int [] binaryArray = new int[100]; //pole pro 100 bitů
+            int [] anyArray = new int[100]; //pole pro 100 bitů
             int index = 0;
 
             if(decimalNumber == 0){
-                binaryArray[index++] = 0;
+                anyArray[index++] = 0;
             } else{
                 while(decimalNumber > 0){
-                    binaryArray[index++] = decimalNumber % 2; //přídání zbytku z dělení na začátek
-                    decimalNumber /= 2; //dělení čísla 2                
+                    anyArray[index++] = decimalNumber % baseNumber; //přídání zbytku z dělení na začátek
+                    decimalNumber /= baseNumber; //dělení čísla 2                
                }
            }
 
             for(int i = index - 1; i >= 0; i--){ //vypis pole pozpatku
-                Console.Write(binaryArray[i]);
-                
-                if(i % 4 == 0){
+                Console.Write(anyArray[i]);
+                if(baseNumber == 2){
+                    if(i % 4 == 0){
+                        Console.Write(" ");
+                    }
+                } else if(i % 3 == 0){
                     Console.Write(" ");
-                }
+                }    
             } 
             
 
