b848f8be4aaa0d1489ea0df981bcc9b9a467738c
Daniel Kolář <daniel.kolar@tul.cz>
2024-11-28 09:53:34 +0100

    Úkol 015 - kombinovaná úloha #15 - vytvoření

--- IS - projekty/program001-výpis-řady/obj/Debug/net8.0/program001-výpis-řady.AssemblyInfo.cs
+++ IS - projekty/program001-výpis-řady/obj/Debug/net8.0/program001-výpis-řady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program001-výpis-řady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c128d381bb24bd61b80fb74d8fab9f4b11a40b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ed507aac986f312678a80cf27abfc7282cb7dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("program001-výpis-řady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program001-výpis-řady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
